153cc3c2acdb9f265e4dbd690204b94d05482673
tristan-oks <98077677+tristan-oks@users.noreply.github.com>
2022-02-07 17:30:13 +0100

    use of ISymptomReader.java
    i rewrited the code using the classes ReadSymptomFromDataFile and IsymptomReader.java
    for now stil WIP, with console output to verify how things go.
    TODO : create the map file containing symptoms in keys with occurences, use this map to create the file result.out

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,42 +3,43 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
 
+/**
+ * 
+ * @author ------
+ *
+ */
 public class AnalyticsCounter {
 	private static int headCount = 0;	// initialize to 0 // nom corrigé
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
+	private static ReadSymptomDataFromFile symptomData = new ReadSymptomDataFromFile("symptoms.txt");
+	private static List<String> symptomsList = symptomData.GetSymptoms();
+	
 	public static void main(String args[]) throws Exception {
-		try (// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"))) {
-			String line = reader.readLine();
+
+			//String line = reader.readLine();
 
 			//int i = 0;	// set i to 0 // inutile
 			//int headCount = 0;	// counts headaches //devenu inutile
-			while (line != null) {
-				//i++;	// increment i // inutile
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headCount++;
-					System.out.println("number of headaches: " + headCount);
-				}
-				else if (line.equals("rash")) //typo corrigé
-					{
-					rashCount++;
-				}
-				else if (line.contains("pupils")) {
-					pupilCount++;
-				}
-
-				line = reader.readLine();	// get another symptom
+		symptomsList.sort(null); // tri alphabetique de la liste des symptomes. Va rendre le comptage beaucoup plus facile
+		String oldSymptom = "";
+		for (String line : symptomsList) {
+				if (line.equals(oldSymptom)) {System.out.println("same symptom : " + line);}
+				else {System.out.println("New symptom : " + line);
+					oldSymptom = line;
+					System.out.println("oldsymptom : " + oldSymptom);}
 			}
-		}
+		
 		// next generate output
+	try (// first get input
+	BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"))) {
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("pupils: " + pupilCount + "\n");
 		writer.close();
 	}
-}
+}}
