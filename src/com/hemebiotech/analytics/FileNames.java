131c9c6721e1dd475026aafd339bce297d6fc8dd
tristan-oks <98077677+tristan-oks@users.noreply.github.com>
2022-02-23 16:51:15 +0100

    class FileNames
    création de la classe Filenames pour gérer les noms des fichiers entrée sortie

---
+++ src/com/hemebiotech/analytics/FileNames.java
@@ -0,0 +1,26 @@
+package com.hemebiotech.analytics;
+
+/**
+ * 
+ * Class containing the names of input and output files
+ * 
+ * @author oksenhendler tristan
+ *
+ */
+public class FileNames {
+  String input;
+  String output;
+
+  /**
+   * 
+   * This constructor instanciate the class with the 2 files names
+   *
+   * @param input input file name
+   * @param output output file name
+   *
+   */
+  FileNames(String input, String output) {
+    this.input = input;
+    this.output = output;
+  }
+}
